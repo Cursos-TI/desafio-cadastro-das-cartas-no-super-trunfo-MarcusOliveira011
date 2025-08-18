67e8c446b218bc7e37160dce517770c24dab2e69
Marcus Oliveira <marcos.v27@hotmail.com>
2025-08-18 05:06:13 +0000

    Criei as Cartas 1 e 2 do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,40 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    char Variavel1 = 'A';
+    char Variavel2[20] = "A01";
+    char Variavel3[20] = "São Paulo";
+    int Variavel4 = 12325000;
+    float Variavel5 = 1521.11;
+    float Variavel6 = 699.28;
+    int Variavel7 = 50;
+
+    char Variavel8 = 'B';
+    char Variavel9[20] = "A02";
+    char Variavel10[20] = "Rio de Janeiro";
+    int Variavel11 = 6748000;
+    float Variavel12 = 1200.25;
+    float Variavel13 = 300.50;
+    int Variavel14 = 30;
+
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", Variavel1);
+    printf("Código: %s\n", Variavel2);
+    printf("Nome da Cidade: %s\n", Variavel3);
+    printf("População: %d\n", Variavel4);
+    printf("Área: %.2f km²\n", Variavel5);
+    printf("PIB: %.2f bilhões de reais\n", Variavel6);
+    printf("Número de Pontos Turísticos: %d\n\n", Variavel7);
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", Variavel8);
+    printf("Código: %s\n", Variavel9);
+    printf("Nome da Cidade: %s\n", Variavel10);
+    printf("População: %d\n", Variavel11);
+    printf("Área: %.2f km²\n", Variavel12);
+    printf("PIB: %.2f bilhões de reais\n", Variavel13);
+    printf("Número de Pontos Turísticos: %d\n", Variavel14);
 
     return 0;
 }
